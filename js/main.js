ef18975b2cd0ad3af937f44ba1e12160942b3d71
k16e <kb@k16e.co>
2019-06-24 12:29:26 +0100

    After setup...

--- js/main.js
+++ js/main.js
@@ -1,31 +1,6 @@
 // All Custom JS
-    // @package kom_v1
+    // @package chl_v1
 
 // Selector shortening helpers
 function q(selector, parent) { return (parent ? parent : document).querySelector(selector); };
 function qq(selector, parent) { return Array.from((parent ? parent : document).querySelectorAll(selector)); };
-
-(function () {
-    'use strict'
-    
-    let
-        topbar    = q('.topbar'),
-        hero      = q('.hero-image'),
-        nav       = q('.mainnav'),
-        height    = hero.getBoundingClientRect().height,
-        navHeight = nav.getBoundingClientRect().height;
-
-    window.addEventListener('scroll', showTopBar, false);
-    
-    function showTopBar(e) {
-        if (window.pageYOffset > height) {
-            topbar.classList.add('is-fixed');
-        } else if (window.pageYOffset < navHeight) {
-            topbar.classList.add('removing-fixed');
-            window.setTimeout(() => {
-                topbar.classList.remove('is-fixed');
-                topbar.classList.remove('removing-fixed');
-            }, 500);
-        }
-    };
-})();
