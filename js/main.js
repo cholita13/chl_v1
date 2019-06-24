46b80b4a93eb7b2abb2824c0a252c6b563e0ee20
k16e <kb@k16e.co>
2019-06-24 17:50:52 +0100

    Completed the header...

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@
 // Selector shortening helpers
 function q(selector, parent) { return (parent ? parent : document).querySelector(selector); };
 function qq(selector, parent) { return Array.from((parent ? parent : document).querySelectorAll(selector)); };
+
+
+// Hide nav after scrolled to section completes
+UIkit.util.on('.scroll-trigger', 'scrolled', () => {
+	UIkit.offcanvas("#offcanvas-reveal").hide();
+});
